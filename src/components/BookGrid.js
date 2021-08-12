3b1719a4ea53eaa3c622eaae9052ff6395c0974e
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2021-08-12 09:03:42 +0200

    Fixed review comments 1. Added the to the README how the app can be installed and run 2. Made the search path render to /search instead of /add-book 3. Fixed the shelf options to highlight the right category if book is found in the main page or shelf [e.g. currentlyReading, wantToRead, read] 4. Refactored the app to add another components for proper segration

--- src/components/BookGrid.js
+++ src/components/BookGrid.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import ShelfOptions from './ShelfOptions';
+import BookFilter from './BookFilter';
 
 const BookGrid = (props) => {
+    // const renderMainListContent = () => {
+
+    //     const content = props.books && props.books.length > 0 ? props.books.map((iBook, i) => (
+    //         <li key={i}>
+    //             <div className="book">
+    //                 <div className="book-top">
+    //                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${iBook.imageLinks && iBook.imageLinks.thumbnail ? iBook.imageLinks.thumbnail : ''}")` }}></div>
+    //                     <div className="book-shelf-changer">
+    //                         <ShelfOptions
+    //                             book={iBook}
+    //                             books={props.books}
+    //                             oldBooks={props.oldBooks}
+    //                             newBooks={props.newBooks}
+    //                             moveBookToShelf={props.moveBookToShelf}
+    //                             renderList={props.renderList} />
+    //                     </div>
+    //                 </div>
+    //                 <div className="book-title">{iBook.title ? iBook.title : 'No Title'}</div>
+    //                 <div className="book-authors">{iBook.authors ? iBook.authors : 'No Author(s)'}</div>
+    //             </div>
+    //         </li>
+    //     )) : null
+    //     return content;
+    // }
+
+    // const renderSearchListContent = () => {
+    //     const content = props.newBooks && props.newBooks.length > 0 ? props.newBooks.map((jBook, i) => (
+    //         <li key={i}>
+    //             <div className="book">
+    //                 <div className="book-top">
+    //                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${jBook.imageLinks && jBook.imageLinks.thumbnail ? jBook.imageLinks.thumbnail : ''}")` }}></div>
+    //                     <div className="book-shelf-changer">
+    //                         <ShelfOptions
+    //                             book={jBook}
+    //                             books={props.newBooks}
+    //                             oldBooks={props.oldBooks}
+    //                             newBooks={props.newBooks}
+    //                             moveBookToShelf={props.moveBookToShelf}
+    //                             bookOnShelf={props.bookOnShelf}
+    //                             renderList={props.renderList} />
+    //                     </div>
+    //                 </div>
+    //                 <div className="book-title">{jBook.title ? jBook.title : 'No Title'}</div>
+    //                 <div className="book-authors">{jBook.authors ? jBook.authors : 'No Author(s)'}</div>
+    //             </div>
+    //         </li>
+    //     )) : null;
+
+    //     return content;
+    // }
+
+    // const ret = function () {
+    //     let view
+    //     if (props.renderList === 'main') {
+    //         view = renderMainListContent()
+    //     } else {
+    //         view = renderSearchListContent()
+    //     }
+    //     return view;
+    // }
     return (
         <ol className="books-grid">
             {
-                props.books ? props.books.map((iBook, i) => (
-                    <li key={i}>
-                        <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${iBook.imageLinks && iBook.imageLinks.thumbnail ? iBook.imageLinks.thumbnail : ''}")` }}></div>
-                                <div className="book-shelf-changer">
-                                    <ShelfOptions
-                                        book={iBook}
-                                        books={props.books}
-                                        moveBookToShelf={props.moveBookToShelf} />
-                                </div>
-                            </div>
-                            <div className="book-title">{iBook.title ? iBook.title : 'No Title'}</div>
-                            <div className="book-authors">{iBook.authors ? iBook.authors : 'No Author(s)'}</div>
-                        </div>
-                    </li>
+                props.books && props.books.length > 0 ? props.books.map((iBook, i) => (
+                    <BookFilter
+                        book={iBook}
+                        books={props.books}
+                        moveBookToShelf={props.moveBookToShelf}
+                        renderList={props.renderList} />
                 )) : null
             }
         </ol>
